94accd01d6ddab6ed3f741d5942e0d285f309438
Ruben Cancino <rubencancino6@gmail.com>
2016-06-21 10:34:00 -0500

    fix 2

--- src/app/luxor/luxor.constant.js
+++ src/app/luxor/luxor.constant.js
@@ -3,7 +3,7 @@
   angular
     .module('sxBi')
     .constant('Config', {
-      'API_ENDPOINT': 'https://papel.dyndns-remote.com/api'
-      //'API_ENDPOINT': 'http://localhost:8080/api'
+      //'API_ENDPOINT': 'https://papel.dyndns-remote.com/api'
+      'API_ENDPOINT': 'http://localhost:8080/api'
     });
 })();
